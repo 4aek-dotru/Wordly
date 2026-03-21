7fdaba6a47e1d034be10b17260bb402e7d6ece31
4aek-dotru <andrey12112005@yandex.ru>
2026-03-21 16:57:21 +0300

    edit

--- main.mjs
+++ main.mjs
@@ -1,1 +1,19 @@
-import Game from 'game.mjs';
+import Game from './game.mjs';
+document.getElementById('start-game-button').onclick = e => {
+    document.getElementById('main-menu-container').style.display = 'none';
+
+    document.getElementById('game-container').style.display = 'flex';
+    window._game = new Game();
+}
+document.getElementById('leader-board-button').onclick = e => {
+    document.getElementById('main-menu-container').style.display = 'none';
+
+    document.getElementById('leader-board-container').style.display = 'flex';
+
+}
+document.getElementById('back-to-main-menu').onclick = e => {
+    document.getElementById('main-menu-container').style.display = 'flex';
+
+    document.getElementById('leader-board-container').style.display = 'none';
+
+}
